b93a72f39cd22de27c0b19f3e3df4ae85387a45f
thinkingjoey <cyleung.berkeley@gmail.com>
2020-09-25 16:16:15 -0700

    first commit
    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$("#page1-next").click(function(){
+  $("#page1").hide();
+  $("#page2").show();
+
+});
+
+$("#page2-next").click(function(){
+  $("#page2").hide();
+  $("#page3").show();
+
+});
+
+$("#page3-next").click(function(){
+  $("#page3").hide();
+  $("#page1").show();
+
+});
